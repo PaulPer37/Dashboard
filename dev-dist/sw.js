37f9d935e2e5773adbb70aced6dc8ffe66bf5894
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2025-07-23 03:14:30 +0000

    a

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "index.html",
-    "revision": "0.6e0d94g0bu"
+    "revision": "0.keogk64ufto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
