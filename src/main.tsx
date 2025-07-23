bbd5ded3e42afb87a6947943f3adca06b9c7c881
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2025-07-23 06:21:04 +0000

    cambios

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'   // <-- Importa BrowserRouter
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Dashboard">    {/* <-- Envuelve y pon basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
